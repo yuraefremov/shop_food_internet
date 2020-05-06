94a6eaee4b22e1ba9ca3caca883d3f2548117c1a
yuraef <18228@inbox.ru>
2020-05-06 17:33:38 +0600

    created an index controller; added the path to the index. Created a view for the index

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,31 @@
+class DishesController < ApplicationController
+  def index
+    @dishes =  Dish.all
+  end
+
+  def show
+    @dish = Dish.find(params[:id])
+  end
+
+  def create
+    @dish = Dish.new(dish_params)
+    if dish.save
+      redirect_to @dish, success: 'Блюдо успешно создано'
+    else
+      render :new
+    end
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+  end
+
+  def delete
+  end
+
+  private 
+
+  def dish_params
+    params.require(:dish).permit(:name, :price, :description)
+  end
+end
