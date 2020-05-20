f92c464e81f8186fcda7f22ae1ff4fbff0f1d211
yuraef <18228@inbox.ru>
2020-05-20 12:18:12 +0600

    fixed errors in the show, made edits, updates, created a view for them

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,10 +1,15 @@
 class DishesController < ApplicationController
+
+  before_action :set_dish, only:[:show, :edit, :update, :destroy ]
   def index
     @dishes =  Dish.all
   end
 
   def show
-    @dish = Dish.find(params[:id])
+  end
+
+  def new
+    @dish = Dish.new
   end
 
   def create
@@ -16,14 +21,28 @@ class DishesController < ApplicationController
     end
   end
 
-  def update 
-    @post = Post.find(params[:id])
+  def edit
   end
 
-  def delete
+  def update 
+    if @dish.update(dish_params)
+      redirect_to @dish, success: 'Блюдо успешно изменено'
+    else
+      render :edit 
+    end
   end
+
+  def destroy
+    @dish.destroy
+    redirect_to root_path, success: 'Блюдо успешно удалено'
+  end
+  
 
   private 
+
+  def set_dish
+     @dish = Dish.find(params[:id])
+  end
 
   def dish_params
     params.require(:dish).permit(:name, :price, :description)
